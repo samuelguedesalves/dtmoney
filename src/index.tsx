0c7f763755af8f38ca79fce81c5f31a22b856421
samuel guedes <guedes.works7@gmail.com>
2022-04-02 13:48:41 -0400

    feat: install and config Mirage JS with Axios

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Almoço',
+          amount: 24.50,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date(),
+        }
+      ]
+    });
+  }
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
